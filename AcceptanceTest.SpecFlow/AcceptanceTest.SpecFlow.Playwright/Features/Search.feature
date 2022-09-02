50859061fcc4d5fd5640d778d23e86fa5990e57b
Francisco Lopez <francisco.lopez@cytosmart.com>
2022-09-02 14:18:30 +0200

    added example feature and step definitions using cucumber expressions

---
+++ AcceptanceTest.SpecFlow/AcceptanceTest.SpecFlow.Playwright/Features/Search.feature
@@ -0,0 +1,11 @@
+﻿Feature: My Example
+
+A short summary of the feature
+
+@Search
+Scenario: This is one scenario example
+	Given Given a navigate to "www.google.com"
+	When I click in the search bar
+	And I type "pokemon" in the search box
+	And I click on the search button
+	Then I should get redirected to a search result page
---
+++ AcceptanceTest.SpecFlow/AcceptanceTest.SpecFlow.Playwright/StepDefinitions/MyExampleStepDefinitions.cs
@@ -0,0 +1,39 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace AcceptanceTest.SpecFlow.Playwright.StepDefinitions
+{
+    [Binding]
+    public class MyExampleStepDefinitions
+    {
+        [Given("Given a navigate to {string}")]
+        public void GivenGivenANavigateTo(string p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [When("I click in the search bar")]
+        public void WhenIClickInTheSearchBar()
+        {
+            throw new PendingStepException();
+        }
+
+        [When("I type {string} in the search box")]
+        public void WhenITypeInTheSearchBox(string pokemon)
+        {
+            throw new PendingStepException();
+        }
+
+        [When("I click on the search button")]
+        public void WhenIClickOnTheSearchButton()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then("I should get redirected to a search result page")]
+        public void ThenIShouldGetRedirectedToASearchResultPage()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
