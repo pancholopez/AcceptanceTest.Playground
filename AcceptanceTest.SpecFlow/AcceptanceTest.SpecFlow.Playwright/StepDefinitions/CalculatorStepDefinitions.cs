015f10fc2803da44456937c12f86ae7d14b7d01e
Francisco Lopez <francisco.lopez@cytosmart.com>
2022-09-02 12:55:49 +0200

    simple specflow example agains a class library

---
+++ AcceptanceTest.SpecFlow/AcceptanceTest.SpecFlow.Playwright/StepDefinitions/CalculatorStepDefinitions.cs
@@ -0,0 +1,36 @@
+using AcceptanceTest.ClassLibrary;
+using NUnit.Framework;
+
+namespace AcceptanceTest.SpecFlow.Playwright.StepDefinitions
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+        private readonly Calculator _calculator = new Calculator();
+        private int _result;
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            _calculator.FirstNumber = number;
+        }
+
+        [Given("the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+            _calculator.SecondNumber = number;
+        }
+
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            _result = _calculator.Add();
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            Assert.AreEqual(result, _result);
+        }
+    }
+}
