8e2d02d9865128f1782e2f60e7cc667e1d962201
Francisco Lopez <francisco.lopez@cytosmart.com>
2022-09-02 13:57:23 +0200

    added support for hooks

---
+++ AcceptanceTest.SpecFlow/AcceptanceTest.SpecFlow.Playwright/Hooks/CalculatorHooks.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+namespace AcceptanceTest.SpecFlow.Playwright.Hooks;
+
+[Binding]
+public class CalculatorHooks
+{
+    private readonly ScenarioContext _scenarioContext;
+
+    public CalculatorHooks(ScenarioContext scenarioContext)
+    {
+        _scenarioContext = scenarioContext;
+    }
+
+    [BeforeFeature]
+    public static void SetupFeature()
+    {
+        Debug.WriteLine("[BeforeFeature] SetupFeature()");
+    }
+
+    [AfterFeature]
+    public static void TeardownFeature()
+    {
+        Debug.WriteLine("[AfterFeature] TeardownFeature()");
+    }
+
+    [BeforeScenario]
+    public void SetupScenario()
+    {
+        Debug.WriteLine("[BeforeScenario] SetupScenario()");
+    }
+
+    [AfterScenario]
+    public void TeardownScenario()
+    {
+        Debug.WriteLine("[AfterScenario] TeardownScenario()");
+    }
+}
