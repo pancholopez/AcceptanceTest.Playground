d24d775c7d04b945c0c668b4c612c1d67f0b1448
Francisco Lopez <francisco.lopez@cytosmart.com>
2022-09-09 16:05:02 +0200

    initial commit for acceptance framework

---
+++ AcceptanceTest.Framework/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace AcceptanceTest.Framework;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
